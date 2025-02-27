7001fa56db42fd775f0303a13cc3be887456e06d
kirilanastasoff <kirilanastasoff@gmail.com>
2025-02-27 15:58:56 +0200

    added 4 more categories

---
+++ crystal18.js
@@ -0,0 +1,19 @@
+let productType = "Продукти от Оникс"; // Define the variable
+
+
+// Fetch products from the backend
+fetch(`${window.config.URL}/api/products/type/${encodeURIComponent(productType)}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch products");
+    }
+    return response.json();
+  })
+  .then(products => {
+    allProducts = products;
+    displayProducts(currentPage);
+  })
+  .catch(error => console.error("Error fetching products:", error));
+
+
+
